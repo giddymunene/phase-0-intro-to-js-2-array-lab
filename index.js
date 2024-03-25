f7a155a5ace4a4db99e04044e5480270b06d81f3
gideon <gideonmunene0@gmail.com>
2024-03-25 11:57:14 -0400

    cats

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+
+const cats =["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+});
+  function destructivelyAppendCat(name){
+  return cats.push(name);}
+  function destructivelyPrependCat(name){
+    return cats.unshift(name)
+  }
+  function destructivelyRemoveFirstCat(){
+    return cats.shift();
+  }
+  function destructivelyRemoveLastCat(){
+    return cats.pop();
+  }
+  function appendCat(name){
+    return [...cats,name]
+  }
+  function prependCat(name){
+    return [name,...cats]
+  }
+  function removeLastCat(){
+    return cats.slice(0,2)
+  }
+  function removeFirstCat(){
+    return cats.slice(1)
+  }
+  console.log(removeLastCat())
+  console.log(prependCat("bob"))
+  console.log (appendCat("Ralph"))
+  console.log(destructivelyAppendCat("Ralph"))
+
